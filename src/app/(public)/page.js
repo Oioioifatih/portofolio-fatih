0f64ac28eab30b8badc923cffd53e2c274d575d6
Oioioifatih <kikialfatih28@gmail.com>
2024-10-07 21:12:10 +0700

    pertemuan 4

--- src/app/(public)/page.js
+++ src/app/(public)/page.js
@@ -1,8 +1,27 @@
-export default function Dashboard() {
-    return (
-      <>
-        asdf public
-      </>
-    );
-  }
-  
+export default function Home() {
+  return (
+    <>
+      <div className="">
+        <h1 className="mt-24 text-6xl font-sans md:Apple Color Emoji">Hallo, </h1>
+        <h1 className=" text-6xl font-sans md:Apple Color Emoji text-yellow-600/100 " > Rizqi Al-Fatih </h1>
+        <h1 className=" text-6xl font-sans md:Apple Color Emoji" >Data Analys</h1>
+        <div>
+          <button className=" mt-20 rounded-lg bg-red-500 w-48 min-h-12 text-white" >Get Started</button>
+        </div>
+      </div>
+
+      
+      
+      <br></br>
+      <br></br>
+      <h2 className="mt-20 rounded-lg text-center text-3xl w-full">Tentang Saya</h2>
+            
+            <p className="text-center margin-0 mx-auto w-2/3	">
+                Saya adalah mahasiswa semester 5 bernama Rizqi Al-Fatih dengan jurusan manajemen informatika tinkat 3 yang kepribadian baik
+            </p>
+
+    </>
+    
+    
+  );
+}
